b957bea8dcbda7f43353405c7ec2d305c8afa072
whtrianto <wahyutri1771@gmail.com>
2024-11-28 01:08:21 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,10 @@
 <?php
-$kon = mysqli_connect("localhost", "root", "", "disleksia", 3306);
-if (mysqli_connect_errno()) {
-	echo "Failed to Connect to Mysql : " . mysqli_connect_error();
-}
+$host = 'sql.freedb.tech'; // misalnya '123.45.67.89' atau nama host yang disediakan
+$username = 'freedb_dislek'; // misalnya 'root' atau nama pengguna database Anda
+$password = 'gF5ndZfA7@8d*4*'; // password yang diberikan oleh penyedia hosting
+$dbname = 'freedb_disleksia'; // nama database yang Anda buat di server online
+$port = 3306; // port default untuk MySQL adalah 3306
+
+// Koneksi ke database
+$kon = mysqli_connect($host, $username, $password, $dbname, $port);
+?>
