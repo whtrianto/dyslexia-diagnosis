789178e6090d983fb32bc6753f66e70316b20f72
whtrianto <wahyutri1771@gmail.com>
2025-07-17 23:43:24 +0700

    Replace all files with new version

--- panduan.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-<!-- Hero Section -->
-<section class="container">
-  <img src="./image/panduan.png" style="width: 100%; 
-      border-radius: 15px;">
-</section>
-<br>
-<br><br>
-<?php include 'footer.php'; ?>
